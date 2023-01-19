e191cff24f87f2a6b864ab0ffa41eeba2a7b88bc
anngelicafernanda <devanngelc@gmail.com>
2023-01-19 16:30:33 -0300

    Utilizando useCallback y useRef en el resumen de cotización

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,40 +1,50 @@
-import useCotizador from "../hooks/useCotizador"
-import { MARCAS, PLANES } from "../constants/Index"
+import { useCallback, useMemo, useRef } from "react";
+import useCotizador from "../hooks/useCotizador";
+import { MARCAS, PLANES } from "../constants/Index";
 
 const Resultado = () => {
-    const {resultado, datos} = useCotizador()
-    const {marca, plan, year} = datos
+  const { resultado, datos } = useCotizador();
+  const { marca, plan, year } = datos;
 
-    const [nombreMarca] = MARCAS.filter( m => m.id === Number(marca))
-    const [nombrePlan] = PLANES.filter( p => p.id === Number(plan))
+  const yearRef = useRef(year)
 
-    if (resultado === 0) {
-        return null
-    }
+  const [nombreMarca] = useCallback(
+    MARCAS.filter((m) => m.id === Number(marca)),
+    [resultado]
+  );
+  const [nombrePlan] = useCallback(
+    PLANES.filter((p) => p.id === Number(plan)),
+    [resultado]
+  );
 
-    return (
+
+
+  if (resultado === 0) {
+    return null;
+  }
+
+  return (
     <div className="bg-gray-100 text-center mt-5 p-5 shadow">
-        <h2 className="text-gray-700 font-black text-3xl"> Resumen</h2>
-    <p className="my-2">
+      <h2 className="text-gray-700 font-black text-3xl"> Resumen</h2>
+      <p className="my-2">
         <span className="font-bold">Marca: </span>
         {nombreMarca.nombre}
-    </p>
-    <p className="my-2">
+      </p>
+      <p className="my-2">
         <span className="font-bold">Plan: </span>
         {nombrePlan.nombre}
-    </p>
+      </p>
 
-    <p className="my-2">
+      <p className="my-2">
         <span className="font-bold">Año del Auto: </span>
-        {year}
-    </p>
-    <p className="my-2 text-2xl">
+        {yearRef.current}
+      </p>
+      <p className="my-2 text-2xl">
         <span className="font-bold">Total Cotización </span>
         {resultado}
-    </p>
-    
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Resultado
+export default Resultado;
