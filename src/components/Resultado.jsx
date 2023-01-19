67b76a855675be1570db7417ab1520d2a3252921
anngelicafernanda <devanngelc@gmail.com>
2023-01-19 16:06:40 -0300

    Mostrando el Resumen de resultado completo

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -6,8 +6,8 @@ const Resultado = () => {
     const {marca, plan, year} = datos
 
     const [nombreMarca] = MARCAS.filter( m => m.id === Number(marca))
-    console.log(nombreMarca)
-    
+    const [nombrePlan] = PLANES.filter( p => p.id === Number(plan))
+
     if (resultado === 0) {
         return null
     }
@@ -19,6 +19,20 @@ const Resultado = () => {
         <span className="font-bold">Marca: </span>
         {nombreMarca.nombre}
     </p>
+    <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+    </p>
+
+    <p className="my-2">
+        <span className="font-bold">Año del Auto: </span>
+        {year}
+    </p>
+    <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotización </span>
+        {resultado}
+    </p>
+    
     </div>
   )
 }
